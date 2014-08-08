dd6d15cf6a90a3dd6d8b5208c267a764d3cae504
David Campos <DAVIDCAMPOS@Davids-MacBook-Pro-3.local>
2014-08-07 21:44:24 -0400

    database credentials added

--- db.js
+++ db.js
@@ -1,11 +1,24 @@
 
+// dbConfig = {
+//   client: 'pg',
+//   connection: {
+//     host     : 'localhost',
+//     user     : 'DAVIDCAMPOS',
+//     password : 'root',
+//     database : 'rush',
+//     charset  : 'utf8',
+//     port     : 5432
+//   }
+// }
+
+
 dbConfig = {
   client: 'pg',
   connection: {
-    host     : 'localhost',
-    user     : 'DAVIDCAMPOS',
-    password : 'root',
-    database : 'rush',
+    host     : 'ec2-54-204-45-126.compute-1.amazonaws.com',
+    user     : 'jgwerhvrludybd',
+    password : 'J-SPIH8ypXQmwSVvr52QOJXOUx',
+    database : 'd63020o634kj1v',
     charset  : 'utf8',
     port     : 5432
   }
